6a6d02372175da28ec6a5a8496cb0b0bf4323cbe
ogirians <ogi.rians3@gmail.com>
2023-03-19 16:56:08 +0700

    second

---
+++ pages/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <v-container fluid class="mx-0 px-0 pt-0 mb-10">
+      <v-carousel 
+      :show-arrows="false" 
+      hide-delimiter-background
+      delimiter-icon="mdi mdi-circle"
+      light
+      height="400"
+      cycle 
+      interval = "1000"
+      class="ma-0"
+      >
+        <v-carousel-item
+          class="mt-n16"
+          v-for="(item,i) in items"
+          :key="i"
+          :src="item.src"
+          cover
+        ></v-carousel-item>
+      </v-carousel>
+    </v-container>
+    <v-container class="text-center mb-16">
+        <h4 class="text-h5 text-md-h4 mb-2">
+          <b>
+            Find your new friend in your location
+          </b>
+        </h4>
+        <h4 style="color: gray;">
+          choose your favorite pet near your place with our app. And dont forget to rate us
+        </h4>
+    </v-container>
+    <v-container class="text-center">
+        <v-btn dark class="rounded-lg primary" style ="text-transform: none;">
+          Masuk
+        </v-btn>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import colors from 'vuetify/es5/util/colors';
+
+export default {
+  name: 'IndexPage',
+  data(){
+    return {
+      items: [
+          {
+            src: '/pic/1.jpg',
+          },
+          {
+            src: '/pic/2.jpg',
+          },
+          {
+            src: '/pic/3.jpg',
+          },
+          {
+            src: '/pic/4.jpg',
+          },
+        ]
+    }
+  }
+}
+</script>
+<style>
+.v-btn > .v-btn__content .v-icon {
+  color: #1976d2;
+}
+</style>
